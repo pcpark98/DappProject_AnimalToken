689b1022575fb4bbbc47e230a3ddeeef051d877c
Sp.arc <pcpark98@naver.com>
2022-11-11 01:14:28 +0900

    initial setting

---
+++ frontend/config-overrides.js
@@ -0,0 +1,13 @@
+/* config-overrides.js */
+
+module.exports = function override(config, env) {
+  //do stuff with the webpack config...
+
+  config.module.rules.push({
+    test: /\.mjs$/,
+    include: /node_modules/,
+    type: "javascript/auto",
+  });
+
+  return config;
+};
