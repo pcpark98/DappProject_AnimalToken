0ad52406fad5d570876c9ad0017992389b7cfefe
Sp.arc <pcpark98@naver.com>
2022-11-12 16:35:17 +0900

    complete: frontend settings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,39 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Button } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./routes/main";
 
 const App: FC = () => {
+  const [account, setAccount] = useState<string>("");
+  // 지금 접속되어 있는 메타 마스크 계정을 담을 변수.
+
+  const getAccount = async () => {
+    // 메타 마스크를 통해 계정을 가져오는 함수.
+
+    try {
+      if (window.ethereum) {
+        // 메타 마스크가 설치되어 있으면 이게 실행될 것이고, 안되어 있으면 실행 안됨.
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        // 지금 접속되어 있는 메타 마스크 계정의 주소가 나옴.
+
+        setAccount(accounts[0]);
+      } else {
+        // 이더리움이 없으면
+        alert("Install Metamask!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAccount();
+  }, []);
+  useEffect(() => {
+    console.log(account);
+  }, [account]);
+
   return (
     <BrowserRouter>
       <Routes>
--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -1,5 +1,9 @@
 /// <reference types="react-scripts" />
 
+// 윈도우라는 객체에 메타 마스크를 설치하면 이더리움이라는 오브젝트가 생김.
+// 리액트에서는 이더리움 오브젝트를 인식하지 못함.
+// 그래서 타입을 강제로 적어주는 것.
+
 interface Window {
   ethereum: any;
 }
